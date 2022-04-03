a7957be3d8676bb3d1816b9d9f76fe94a219b4fe
Minkyeu Kim <minkyeki@c9r5s7.42seoul.kr>
2022-04-03 10:49:52 +0900

    ft_putstr_fd error handling

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,15 +6,35 @@
 /*   By: minkyeki <minkyeki@42SEOUL.KR>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 14:20:47 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/03/22 15:53:14 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/04/03 10:32:51 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdlib.h> /* for exit() */
 
+/* NOTE : Dangerous if write() error occures!! */
 extern size_t	ft_strlen(const char *str);
 
-void	ft_putstr_fd(char *s, int fd)
+/** void	ft_putstr_fd(char *s, int fd)
+  * {
+  *     write(fd, s, ft_strlen(s));
+  * } */
+
+/* NOTE : Paritial write && write() error handling */
+void	ft_putstr_fd(char *string, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	size_t	idx;
+	size_t	len;
+	ssize_t	writed_bytes;
+
+	idx = 0;
+	len = ft_strlen(string);
+	while (idx < len)
+	{
+		writed_bytes = write(fd, &string[idx], len);
+		if (writed_bytes < 0)
+			exit(EXIT_FAILURE);
+		idx += writed_bytes;
+	}
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: minkyeki <minkyeki@42SEOUL.KR>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 17:45:16 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/03/23 10:52:46 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/04/03 10:47:23 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,8 @@ extern int		ft_isprint(int c);
 /* Return the length of str. */
 extern size_t	ft_strlen(const char *str);
 
-/* Set n bytes of s to 0. */
-extern void		ft_bzero(void *s, size_t n);
+/* Set n bytes of mem to 0. */
+extern void		ft_bzero(void *mem, size_t n);
 
 /* Copy up to dstsize - 1 characters from the NUL-terminated 
  * string src to dest, NUL-terminating the result. */
@@ -61,41 +61,46 @@ extern int		ft_toupper(int c);
 extern int		ft_tolower(int c);
 
 /* Returns a pointer to the first occurrence of 
- * the character c in the string s. */
-extern char		*ft_strchr(const char *s, int c);
+ * the character c in the string str. */
+extern char		*ft_strchr(const char *str, int c);
 
 /* Returns a pointer to the last occurrence of 
- * the character c in the string s. */
-extern char		*ft_strrchr(const char *s, int c);
+ * the character c in the string str. */
+extern char		*ft_strrchr(const char *str, int c);
 
-/* Compare n characters of s1 and s2. */
-extern int		ft_strncmp(const char *s1, const char *s2, size_t n);
+/* Compare n characters of str1 and str2. */
+extern int		ft_strncmp(const char *str1, const char *str2, size_t n);
 
 /* Fill the first len bytes of the memory area 
  * pointed to by b with the constant byte c. */
 extern void		*ft_memset(void *b, int c, size_t len);
 
-/* Copy n bytes from memory area src to memory area dst.
+/* Copy n bytes from memory area src to memory area dest.
  * The memory areas must not overlap.
  * Use ft_memmove if the memory areas do overlap. */
-extern void		*ft_memcpy(void *dst, const void *src, size_t n);
+extern void		*ft_memcpy(void *dest, const void *src, size_t n);
 
-/* Copy len bytes from memory area src to memory area dst.
+/* Copy len bytes from memory area src to memory area dest.
  * The memory areas may overlap: copying takes place as 
  * though the bytes in src are first copied into a temporary array 
  * that does not overlap src or dest, and the bytes are then copied 
  * from the temporary array to dest. */
-extern void		*ft_memmove(void *dst, const void *src, size_t len);
+extern void		*ft_memmove(void *dest, const void *src, size_t len);
 
 /* Scan the initial n bytes of the memory area pointed to 
- * by s for the first instance of c.
+ * by mem for the first instance of c.
  * Both c and the bytes of the memory area pointed to 
- * by s are interpreted as unsigned char. */
-extern void		*ft_memchr(const void *s, int c, size_t n);
+ * by mem are interpreted as unsigned char. 
+ * -
+ * [RETURN VALUES].
+ * The memchr() function returns a pointer to the byte located, 
+ * or NULL if no such byte exists within n bytes.
+ * */
+extern void		*ft_memchr(const void *mem, int c, size_t nbytes);
 
-/* Compares the first n bytes (each interpreted as unsigned char) 
- * of the memory areas s1 and s2. */
-extern int		ft_memcmp(const void *s1, const void *s2, size_t n);
+/* Compares the first nbytes (each interpreted as unsigned char) 
+ * of the memory areas mem1 and mem2. */
+extern int		ft_memcmp(const void *mem1, const void *mem2, size_t nbytes);
 
 /* Locates the first occurrence of the null-terminated string needle
  * in the string haystack, where not more than len characters are searched. */
@@ -110,49 +115,50 @@ extern int		ft_atoi(const char *str);
  * Returns a pointer to the allocated memory, which is set to zero. */
 extern void		*ft_calloc(size_t nmemb, size_t bytes);
 
-/* Return a pointer to a new string which is a duplicate of the string s1. */
-extern char		*ft_strdup(const char *s1);
+/* Return a pointer to a new string which is a duplicate of the string str1. */
+extern char		*ft_strdup(const char *str1);
 
-/* Allocates and returns a substring from the string ’s’.
+/* Allocates and returns a substring from the string ’str’.
  * The substring begins at index ’start’ and is of maximum size ’len’.*/
-extern char		*ft_substr(char const *s, unsigned int start, size_t len);
+extern char		*ft_substr(char const *str, unsigned int start, size_t len);
 
 /* Allocates and returns a new string, which is the result 
- * of the concatenation of ’s1’ and ’s2’ */
-extern char		*ft_strjoin(char const *s1, char const *s2);
+ * of the concatenation of ’str1’ and ’str2’ */
+extern char		*ft_strjoin(char const *str1, char const *str2);
 
-/* Allocates and returns a copy of ’s1’ with the characters 
+/* Allocates and returns a copy of ’str1’ with the characters 
  * specified in ’set’ removed from the beginning and the end of the string. */
-extern char		*ft_strtrim(char const *s1, char const *set);
+extern char		*ft_strtrim(char const *str1, char const *set);
 
 /* Allocates and returns an array of strings obtained by splitting 
  * ’s’ using the character ’c’ as a delimiter. */
-extern char		**ft_split(char const *s, char c);
+extern char		**ft_split(char const *str, char c);
 
 /* Allocates and returns a string representing the integer 
  * received as an argument. */
-extern char		*ft_itoa(int n);
+extern char		*ft_itoa(int num);
 
 /* Allocates and returns a string resulting from successive 
- * applications of ’f’ to each character of the string ’s’.*/
-extern char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+ * applications of ’func’ to each character of the string ’s’.*/
+extern char		*ft_strmapi(char const *string, \
+		char (*func)(unsigned int, char));
 
-/* Applies the function 'f' to each character of the string 
+/* Applies the function 'func' to each character of the string 
  * passed as argument, and passing its index to 'f' as first argument. */
-extern void		ft_striteri(char *s, void (*f)(unsigned int, char*));
+extern void		ft_striteri(char *string, void (*func)(unsigned int, char*));
 
 /* Outputs the character ’c’ to the given file descriptor. */
-extern void		ft_putchar_fd(char c, int fd);
+extern void		ft_putchar_fd(char character, int fd);
 
 /* Outputs the string ’s’ to the given file descriptor. */
-extern void		ft_putstr_fd(char *s, int fd);
+extern void		ft_putstr_fd(char *str, int fd);
 
 /* Outputs the string ’s’ to the given file descriptor, 
  * followed by a newline. */
-extern void		ft_putendl_fd(char *s, int fd);
+extern void		ft_putendl_fd(char *str, int fd);
 
 /* Outputs the integer ’n’ to the given file descriptor. */
-extern void		ft_putnbr_fd(int n, int fd);
+extern void		ft_putnbr_fd(int num, int fd);
 
 /* Bonus Part (Linked List) */
 typedef struct s_list
