4c7fda9ff2abe281946c2ed62627210ae48dc6ab
Minkyeu Kim <minkyeki@c9r5s7.42seoul.kr>
2022-04-03 11:01:24 +0900

    ft_calloc overflow handled

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,15 @@
 /*   By: minkyeki <minkyeki@42SEOUL.KR>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/09 19:26:04 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/03/23 00:10:29 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/04/03 10:57:09 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+
+/* NOTE : code below is an overflow-safe version of ft_calloc */
 
 extern void		ft_bzero(void *s, size_t n);
 
@@ -20,6 +24,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
+	if (nmemb > SIZE_MAX / size)
+	{
+		errno = EOVERFLOW;
+		return (NULL);
+	}
 	tmp = malloc(nmemb * size);
 	if (!tmp)
 		return (NULL);
