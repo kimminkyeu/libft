548c19433f181f221543a1eeac13bc467eff4408
Minkyeu Kim <minkyeki@c1r6s7.42seoul.kr>
2022-04-12 20:29:25 +0900

    update additional function

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,20 +6,14 @@
 /*   By: minkyeki <minkyeki@42SEOUL.KR>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 14:20:47 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/04/03 10:32:51 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/04/12 20:26:41 by minkyeki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h> /* for exit() */
 
 /* NOTE : Dangerous if write() error occures!! */
 extern size_t	ft_strlen(const char *str);
-
-/** void	ft_putstr_fd(char *s, int fd)
-  * {
-  *     write(fd, s, ft_strlen(s));
-  * } */
 
 /* NOTE : Paritial write && write() error handling */
 void	ft_putstr_fd(char *string, int fd)
@@ -34,7 +28,7 @@ void	ft_putstr_fd(char *string, int fd)
 	{
 		writed_bytes = write(fd, &string[idx], len);
 		if (writed_bytes < 0)
-			exit(EXIT_FAILURE);
+			return ;
 		idx += writed_bytes;
 	}
 }
