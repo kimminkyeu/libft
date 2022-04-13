3e6ddffa38171236e16dc4ba9a2a52d7d8be8e27
kimminkyeu <kimminkyeu@gmail.com>
2022-04-13 10:16:09 +0900

    ft_nputstr_fd condition error handled

--- ft_nputstr_fd.c
+++ ft_nputstr_fd.c
@@ -6,17 +6,22 @@
 /*   By: minkyeki <minkyeki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 20:33:21 by minkyeki          #+#    #+#             */
-/*   Updated: 2022/04/12 20:40:39 by minkyeki         ###   ########.fr       */
+/*   Updated: 2022/04/13 10:11:07 by kyeu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include "libft.h"
 
 void	ft_nputstr_fd(const char *string, int fd, size_t n)
 {
 	size_t	idx;
 	ssize_t	writed_bytes;
+	size_t	len;
 
+	len = ft_strlen(string);
+	if (n > len)
+		n = len;
 	idx = 0;
 	while (idx < n)
 	{
